e3ef164b28811c50ebe34fed68d0ca51976b7e8a
sagiegurari <sagiegurari@gmail.com>
2016-09-07 23:32:21 +0300

    use shared lint/build settings

--- project/build/project.js
+++ project/build/project.js
@@ -1,12 +1,10 @@
 'use strict';
 
 module.exports = function (grunt) {
-    grunt.registerTask('integration-test', 'Run integration tests', [
-        'empty'
-    ]);
-
     //load markdownlint task
     grunt.loadTasks('tasks');
 
-    return {};
+    var commons = require('js-project-commons');
+
+    return commons.grunt.project(grunt);
 };
